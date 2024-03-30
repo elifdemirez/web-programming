5778aab439e5dedfdf4edb89ead74c9a84f98b27
elifdemirez <endmrzn@gmail.com>
2024-03-31 02:50:18 +0300

    first commit

---
+++ JAVASCRİPT/script.js
@@ -0,0 +1,128 @@
+// console.log("Hoş geldiniz")
+// console.log(123)
+// console.error("Hata oluştu")
+// console.warn("Bir uyarı oluştu")
+
+// console.clear()
+
+
+
+// Değişkenler
+// var password;
+// console.log(password)
+// password = "123"
+// console.log(password)
+
+// yas = 19;
+// console.log(yas)
+
+// Değişken tanımlama kuralları
+// Sayısal ifade ile başlayamaz
+// Türkçe karakter kullanılamaz
+// Boşluk kullanılamaz
+// "_" kullanılabilir
+// "-" kullanılamaz
+// case sensitive (Büyük küçük harf duyarlılığı)
+// tanımlama yapılırken var, let, const kullanılabilir
+
+// yas1 = 20;
+// var adSoyad = "Elifnur Demirezen"
+// console.log(adSoyad)
+
+// const email="12345@gmail.com"
+// console.log(email)
+//email = "56789@gmail.com"
+//console.log(email)
+
+
+//Değişken Tipleri
+
+// Primitive Types
+
+// String
+// let firstName = "Elifnur"
+// console.log(typeof firstName)
+
+// // Number
+// let age = 19
+// console.log(typeof age)
+
+// // Boolean
+// let isActive = true
+// console.log(typeof isActive)
+
+// //undefined
+// let city;
+// console.log(typeof city)
+
+// //reference Types: Object
+// // Array
+// let names = ['Elif', 'Nur', 'Demirezen']
+// console.log(typeof names)
+
+// //object literal
+// let address = {
+//     city: 'İstanbul',
+//     country: 'Türkiye'
+// }
+// console.log(typeof address)
+
+// var hesapla=function(){
+//     return 0
+// }
+// console.log(typeof hesapla)
+
+
+//OPERATÖRLER
+
+//Aritmetik Operatörler
+// let veri;
+// const a=20;
+// const b=10;
+// let d=3;
+// const c=5;
+
+// veri = a+b;
+// veri = a-b;
+// veri = a*b;
+// veri = a/b;
+// veri=d++;
+// veri=++d;
+// veri=d--;
+// veri=--d;
+// veri=a%b;
+
+// //Atama Operatörleri
+// veri = a;
+// veri +=a;
+// veri -=a;
+// veri *=a;
+// veri /=a;
+// veri %=a;
+
+// //Karşılaştırma Operatörleri
+// veri = a==b;
+// veri = b==c;
+// veri = 5==="5";
+// veri = a!==b;
+// veri = a>b;
+// veri = a<b;
+// veri = a>=b;
+// veri = a<=b;
+
+// //Mantıksal Operatörler
+// veri = (a>b) && (a>c);
+// veri = (a>b) || (a<c);
+// veri = !(a>b);
+
+
+
+// console.log(veri)
+// console.log(typeof veri)
+
+
+// DATE OBJECT
+let zaman = new Date();
+
+console.log(zaman);
+console.log(typeof zaman);
